6c0126ffb4525314fade4792a2ec7d397d2f5489
Abu Nesar <anshahin3@gmail.com>
2020-08-16 15:47:44 +0600

    for testing the app

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+
+	path('login/', views.loginPage, name="login"),
+	path('signup/', views.signupPage, name="signup"),
+	path('logout/', views.logoutUser, name="logout"),
+
+]
